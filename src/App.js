d4b8ad6930bbe24cbb0161cf6c0a688e3dae4f5a
Amy Hua <aymay98@gmail.com>
2021-05-19 15:05:01 -0500

    Connect to React GitHub API

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ import { Card, Icon, Image, Dropdown } from 'semantic-ui-react';
 
 
 function App() {
-  const [name, setName] = useState('');
+  const [watchers, setWatchers] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.github.com/repos/facebook/create-react-app')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        console.log(data.watchers_count)
+        setReact(data);
+      });
+  }, []);
+
+  const setReact = (watchers_count) => {
+    setWatchers(watchers)
+  }
 
   return (
     <div>
       <h2 className="navbar">Github Language Dashboard</h2>
       <div className="card">
       <Card>
+        <Card.Content>
+        <Card.Header>React</Card.Header>
+        </Card.Content>
     <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
     <Card.Content>
-      <Card.Header>Matthew</Card.Header>
       <Card.Meta>
-        <span className='date'>Joined in 2015</span>
+        <span className='watchers'>Watchers: {watchers}</span>
       </Card.Meta>
       <Card.Description>
         Matthew is a musician living in Nashville.
